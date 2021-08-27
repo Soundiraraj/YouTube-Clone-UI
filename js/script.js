c2294ca5b56e17c12a7289e54c291d85de37eb7a
soundiraraj <soundiraraj1110@gmail.com>
2021-08-27 21:53:19 +0530

    Main Sections Complete Successfull

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".siderbar");
+var container = document.querySelector(".container");
 
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
+  container.classList.toggle("large-container");
 }
